dbb0da6a0f12f62bb6b436c66d3f098da352117c
isurojit <iamsurojit@hotmail.com>
2024-03-19 10:35:53 +0530

    player edit state 1

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editBtnClickHandler = (e) => {
+    setIsEditing(true);
+  };
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required placeholder="Change Name" />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={editBtnClickHandler}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
     </li>
   );
 };
