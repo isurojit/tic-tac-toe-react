222ccb197949931d67b89af50d73d0ca97065256
isurojit <iamsurojit@hotmail.com>
2024-03-23 09:27:14 +0530

    Typo Changed

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GameOver = ({ winner, onRestart }) => {
   return (
     <div id="game-over">
-      <h2>Your Game Over</h2>
+      <h2>Game Over</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p>It's a draw!</p>}
       <p>
